be0a8d510f68798f221a4c8f0b9eeee6ceccc4ba
Inês <inesamorimalves@gmail.com>
2019-03-13 14:40:07 +0000

    yay

--- tp.prolog.pl
+++ tp.prolog.pl
@@ -133,11 +133,13 @@ comprimento( [H | T],R ) :- comprimento( T,S ), R is S+1.
 pertence(X,[H | T]) :- X = H.
 pertence(X,[H | T]) :- pertence(X,T),X \= H.
 
+
 % Extensao do predicado apagaT: Elemento,ListaInicial,ListaFinal -> {V,F}
 
 apagaT(_,[],[]).
 apagaT(X,[H | T], [H | L]) :- X \= H, apagaT(X,T,L).
 apagaT(X,[H | T], L) :- apagaT(X,T,L).
+
 
 % Extensao do predicado removeRepetidos: ListaInicial,ListaFinal -> {V,F}
 
@@ -145,10 +147,12 @@ removeRepetidos([],[]).
 removeRepetidos([H | T],R) :- pertence(H,T), apagaT(H,T,S), removeRepetidos(S,B), R = [H|B].
 removeRepetidos([H | T],R) :- not(pertence(H,T)),removeRepetidos(T,S), R = [H|S].
 
+
 % Extensao do predicado somaLista: ListaNum,Resultado -> {V,F}
 
 somaLista( [],0 ).
 somaLista( [H | T],R ) :- somaLista( T,S ), R is H+S.
+
 
 % Extensao do predicado concatenar: ListaX,ListaY,ListaFinal -> {V,F}
 
@@ -185,13 +189,16 @@ teste( [R|LR] ) :- R, teste( LR ).
 
 registar_utente(X,Y,W,Z) :- evolucao(utente(X,Y,W,Z)).
 
+
 % Extensao do predicao registar_servico: IdServico, Descricao, Instituicao, Cidade -> {V,F}
 
 registar_servico(X,Y,W,Z) :- evolucao(servico(X,Y,W,Z)).
 
+
 % Extensao do predicao registar_consulta: Data, IdUtente, IdServico, Custo, IdMedico -> {V,F}
 
 registar_consulta(X,Y,W,Z,M) :- evolucao(consulta(X,Y,W,Z,M)).
+
 
 % Extensao do predicao registar_medico: IdMedico, Nome, Especialidades, Instituicoes -> {V,F}
 
@@ -203,13 +210,16 @@ registar_medico(X,Y,W,Z) :- evolucao(medico(X,Y,W,Z)).
 
 remover_utente(X,Y,W,Z) :- involucao(utente(X,Y,W,Z)).
 
+
 % Extensao do predicao remover_servico: IdServico, Descricao, Instituicao, Cidade -> {V,F}
 
 remover_servico(X,Y,W,Z) :- involucao(servico(X,Y,W,Z)).
 
+
 % Extensao do predicao remover_consulta: Data, IdUtente, IdServico, Custo, IdMedico -> {V,F}
 
 remover_consulta(X,Y,W,Z,M) :- involucao(consulta(X,Y,W,Z,M)).
+
 
 % Extensao do predicao remover_medico: IdMedico, Nome, Especialidades, Instituicoes -> {V,F}
 
@@ -227,72 +237,89 @@ identificaInstituicoes(Lista) :- solucoes(I,servico(_,_,I,_),L),removeRepetidos(
 
 identificar_utenteID(ID, R) :- solucoes((ID, N, I, C), utente(ID, N, I, C), R).
 
+
 % Extensao do predicado identificar_utenteNome: Nome,ListaUtentes -> {V,F}
 
 identificar_utenteNome(NOME, R) :- solucoes((ID, NOME, I, C), utente(ID, NOME, I, C), R).
+
 
 % Extensao do predicado identificar_utenteIdade: Idade,ListaUtentes -> {V,F}
 
 identificar_utenteIdade(IDADE, R) :- solucoes((ID, N, IDADE, C), utente(ID, N, IDADE, C), R).
 
+
 % Extensao do predicado identificar_utenteCidade: Cidade,ListaUtentes -> {V,F}
 
 identificar_utenteCidade(CIDADE, R) :- solucoes((ID, N, I, CIDADE), utente(ID, N, I, CIDADE), R).
+
 
 % Extensao do predicado identificar_servicoID: IDServ,Servico -> {V,F}
 
 identificar_servicoID(ID, R) :- solucoes((ID, D, I, C), servico(ID, D, I, C), R).
 
+
 % Extensao do predicado identificar_servicoDescricao: Descricao,Servico -> {V,F}
 
 identificar_servicoDescricao(DESC, R) :- solucoes((ID, DESC, I, C), servico(ID, DESC, I, C), R).
+
 
 % Extensao do predicado identificar_servicoInstituicao: Instituicao,ListaServicos -> {V,F}
 
 identificar_servicoInstituicao(INST, R) :- solucoes((ID, D, INST, C), servico(ID, D, INST, C), R).
 
+
 % Extensao do predicado identificar_servicoCidade: Cidade,ListaServicos -> {V,F}
 
 identificar_servicoCidade(CITY, R) :- solucoes((ID, D, I, CITY), servico(ID, D, I, CITY), R).
+
 
 % Extensao do predicado identificar_consultaData: Data,ListaConsultas -> {V,F}
 
 identificar_consultaData(DATE, R) :- solucoes((DATE, Idutente, Idservico, Custo,Idmedico), 
                                         consulta(DATE, Idutente, Idservico, Custo,Idmedico), R).
 
+
 % Extensao do predicado identificar_consultaIDUtente: IdUt,ListaConsultas -> {V,F}
 
 identificar_consultaIDUtente(IDU, R) :- solucoes((D, IDU, Idservico, Custo,Idmedico), 
                                             consulta(D, IDU, Idservico, Custo,Idmedico), R).
+
 
 % Extensao do predicado identificar_consultaIDServico: IdServ,ListaConsultas -> {V,F}
 
 identificar_consultaIDServico(IDS, R) :- solucoes((D, Idutente, IDS, Custo,Idmedico), 
                                             consulta(D, Idutente, IDS, Custo,Idmedico), R).
 
+
 % Extensao do predicado identificar_consultaCusto: Custo,ListaConsultas -> {V,F}
 
 identificar_consultaCusto(CUSTO, R) :- solucoes((D, Idutente, Idservico, CUSTO,Idmedico), 
                                             consulta(D, Idutente, Idservico, CUSTO,Idmedico), R).
+
 
 %------------------------5--------------------------
 % Extensao do predicado servicosInstituicao: Instituicao, ListaServicos -> {V,F}
 
 servicosInstituicao(I,Lista) :- solucoes(S,servico(_,S,I,_),R),removeRepetidos(R,Lista).
 
+
 % Extensao do predicado servicosCidade: Cidade, ListaServicos -> {V,F}
 
 servicosCidade(C,Lista) :- solucoes(S,servico(_,S,_,C),R), removeRepetidos(R,Lista).
+
 
 % Extensao do predicado servicosData: Data, ListaServicos -> {V,F}
 
 servicosData(D,Lista) :- solucoes(IDServ,consulta(D,_,IDServ,_,_),R), descServicos(R,T),removeRepetidos(T,Lista).
 
+
 % Extensao do predicado servicosCusto: Custo, ListaServicos -> {V,F}
 
 servicosCusto(C,Lista) :- solucoes(IDServ,consulta(_,_,IDServ,C,_,),L), descServicos(L,T), removeRepetidos(T,Lista).
 
+
 %--- Auxiliar da 5
+
 % Extensao do predicado descServicos: ListaIdServ, ListaServicos -> {V,F}
 
 descServicos([],[]).
@@ -304,9 +331,11 @@ descServicos([H | T],Lista) :- solucoes(S,servico(H,S,_,_),L), descServicos(T,R)
 
 utentesServico(I,S):- solucoes(IDU,consulta(_,IDU,I,_,_,),W),removeRepetidos(W,U),encontrarUtentes(U,S).
 
+
 % Extensao do predicado utentesInstituicao: Instituicao,ListaUtentes -> {V,F}
 
 utentesInstituicao(I,R) :- solucoes(IDS,servico(IDS,_,I,_),S),servicosUtentes(S,R).
+
 
 %--- Auxiliar da 6
 
@@ -314,6 +343,7 @@ utentesInstituicao(I,R) :- solucoes(IDS,servico(IDS,_,I,_),S),servicosUtentes(S,
 
 encontrarUtentes([],[]).
 encontrarUtentes([H|T],R) :- identificar_utenteID(H,W),encontrarUtentes(T,S),concatenar(S,W,R).
+
 
 % Extensao do predicado servicosUtentes: ListaServicos,ListaUtentes -> {V,F}
 
@@ -328,7 +358,8 @@ servicosUtentes([H|T],R) :- utentesServico(H,W),servicosUtentes(T,S),concatenar(
 servicosUtente([],[]).
 servicosUtente(IDU,LR) :- solucoes(IDS,consulta(_,IDU,IDS,_,_),W),removeRepetidos(W,U),encontrarServicoU(U,LR).
 
-%--- auxiliar da 7
+
+%--- Auxiliar da 7
 encontrarServicoU([],[]).
 encontrarServicoU([H|T],R) :- solucoes((H,N,I,M),servico(H,N,I,M),W),encontrarServicoU(T,S),concatenar(S,W,R).
 
@@ -337,17 +368,21 @@ encontrarServicoU([H|T],R) :- solucoes((H,N,I,M),servico(H,N,I,M),W),encontrarSe
 
 custoUtente(ID,R) :- solucoes(C,consulta(_,ID,_,C,_,),L), somaLista(L,R).
 
+
 % Extensao do predicado custoServico: IDServ,Custo -> {V,F}
 
 custoServico(ID,R) :- solucoes(C,consulta(_,_,ID,C,_),L), somaLista(L,R).
+
 
 % Extensao do predicado custoData: Data,Custo -> {V,F}
 
 custoData(Data,R) :- solucoes(C,consulta(Data,_,_,C,_),L), somaLista(L,R).
 
+
 % Extensao do predicado custoInstituicao: Instituicao,Custo -> {V,F}
 
 custoInstituicao(I,R) :- solucoes(IDServ,servico(IDServ,_,I,_),Lista), custoListaServ(Lista,R).
+
 
 %--- Auxiliar da 8
 % Extensao do predicado custoListaServ: ListaServicos,Custo -> {V,F}
